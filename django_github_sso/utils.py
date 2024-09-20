6f3b50d71d26b16bcbb96192eae11cd40faa9262
Pavel Mises <id@dqd.cz>
2024-09-20 10:25:32 +0200

    Usage without django_microsoft_sso installed

--- django_github_sso/utils.py
+++ django_github_sso/utils.py
@@ -1,11 +1,15 @@
 from django.contrib import messages
-from django_microsoft_sso import conf
 from loguru import logger
+
+try:
+    from django_microsoft_sso import conf
+except ImportError:
+    conf = None
 
 
 def send_message(request, message, level: str = "error"):
     getattr(logger, level.lower())(message)
-    if conf.MICROSOFT_SSO_ENABLE_MESSAGES:
+    if conf and conf.MICROSOFT_SSO_ENABLE_MESSAGES:
         messages.add_message(request, getattr(messages, level.upper()), message)
 
 
